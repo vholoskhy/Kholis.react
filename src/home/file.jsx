3240dbeb0a55b4275fa183a94fadb960a4ceb455
koliscom <vholoskhy@gmail.com>
2024-09-07 10:06:07 +0700

    react-2-navbar

---
+++ src/home/file.jsx
@@ -0,0 +1,14 @@
+import "./file.css";
+
+function Description() {
+  return (
+    <div className="container">
+      <p>KHOLIS GANTENG</p>
+      <span>Kota</span>
+      <br />
+      <input type="text" placeholder="pilihan" />
+    </div>
+  );
+}
+
+export default Description;
